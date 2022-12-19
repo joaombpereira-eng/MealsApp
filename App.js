b930e6bf25912bcd554f537e86b030da368ee172
joaombpereira-eng <joaopereira.eng.tec@gmail.com>
2022-12-19 09:39:12 +0000

    Navigation Started

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import {View, StyleSheet, StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
 import CategoriesScreen from './screens/CategoriesScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <StatusBar translucent={true} backgroundColor={'transparent'} />
       <View style={styles.container}>
-        <CategoriesScreen />
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="MealsCategories">
+            <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+            {console.log('Stack Screen')}
+          </Stack.Navigator>
+        </NavigationContainer>
       </View>
     </>
   );
@@ -15,6 +25,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     backgroundColor: '#24180F',
     paddingTop: 20,
   },
